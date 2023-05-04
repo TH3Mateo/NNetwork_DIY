a1c3ae4b6cb6d1493954610b23638e16f0ff8f27
TH3Mateo <th3mateo@gmail.com>
2023-05-04 22:30:20 +0200

    after enabling GPU

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import tensorflow
 import keras
+from numba import jit, cuda
+import numpy as np
 
 print(tensorflow.__version__)
 print(keras.__version__)
+
+
+@jit(target_backend="cuda")
+def func(a):
+    for i in range(10000000):
+        a[i] += 1
+    return a
+
+
+
+
+def main():
+    print("start")
+    func(np.ones(10000000))
+    print("end")
+
+
+main()
